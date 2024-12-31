c3fe0de475e6a196333964d7d8274a8787f67a52
TCcjx <tcc2025@163.com>
2024-12-31 11:28:02 +0800

    third commit

--- 1_dataset_dataloader/10_10/word2index.py
+++ 1_dataset_dataloader/10_10/word2index.py
@@ -6,6 +6,7 @@
  @SoftWare: PyCharm
 """
 import os
+import numpy as np
 def read_data(file):
     all_text = []
     all_label = []
@@ -21,6 +22,42 @@ def read_data(file):
         all_label.append(label)
     return all_text, all_label
 
+class MyDataset:
+    def __init__(self,all_text,all_label,batch_size,shuffle = True):
+        self.all_text = all_text
+        self.all_label = all_label
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+
+        assert  len(all_text) == len(all_label)
+
+    def __iter__(self):
+        return DataLoader(self)
+
+    def __len__(self):
+        return len(self.all_text)
+
+class DataLoader(object):
+    def __init__(self,dataset):
+        self.dataset = dataset
+        self.cursor = 0 # 光标
+        self.shuffle_index = np.arange(len(self.dataset)
+
+        if self.dataset.shuffle == True:
+            np.random.shuffle(self.shuffle_index)
+
+    def __next__(self):
+        if self.cursor >= len(self.dataset):
+            raise StopIteration
+
+        index = self.shuffle_index[self.cursor : self.cursor + self.dataset.batch_size]
+        text = self.dataset.all_text[index]
+        label = self.dataset.all_label[index]
+
+        self.cursor += self.dataset.batch_size
+        return text,label
+
 if __name__ == '__main__':
     train_text, train_label = read_data(os.path.join('..','data','train.txt'))
-    print(train_text,train_label)
+    assert len(train_text) == len(train_label)
+    print(f'数据加载成功，数据长度为{len(train_text)}')
